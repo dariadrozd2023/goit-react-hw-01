4a55b35565e7f74699c1241024c6fa47e231ca8b
daria drozd <daria.drozd1990@gmail.com>
2024-06-27 12:04:59 +0300

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,8 +1,13 @@
 export const Profile = () => {
-    return (
-      <div>
-       
-      </div>
-    );
-  };
-  
+  return (
+    <div>
+      <ul>
+        <li className="name">name</li>
+        <li className="tag">tag</li>
+        <li className="location">location</li>
+        <li className="image">image</li>
+        <li className="stats">stats</li>
+      </ul>
+    </div>
+  );
+};
