d09f50db45976a701d16b8022b3a4589acb09f5c
daria drozd <daria.drozd1990@gmail.com>
2024-06-27 20:37:13 +0300

    ../friends.json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,21 @@
-import { Profile } from "./Profile/Profile";
-
+import { Profile } from './Profile/Profile';
+import userData from '../userData.json';
+import friends from "../friends.json";
+import {FriendListItem} from './FriendListItem/FriendListItem'
+// import {FriendList}  from './FriendList/FriendList';
 
 export default function App() {
-  return (
+  return ( 
     <>
-   <Profile/>
-    </>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+ <FriendListItem  />
+
+  </>
   );
 }
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,22 @@
+// import { FriendListItem } from '../FriendListItem/FriendListItem';
+// import friends from "../friends.json";
+
+
+
+// export function FriendList() {
+//   return (
+//     <div>
+//       <ul>
+//         {/* Кількість li залежить від кількості об'єктів в масиві */}
+//         <li>
+//           <FriendListItem
+//           name={friends.name}
+//           avatar={friends.avatar}
+//           isOnline={friends.isOnline}
+//            />
+//         </li>
+    
+//       </ul>
+//     </div>
+//   );
+// }
